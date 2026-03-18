197c56f95359629ed570746da684db01c043d4a9
hazemammar <hazemamaar9@gmail.com>
2026-03-18 16:15:05 +0200

    Init Video Games App

--- app/src/main/java/com/example/videogames/utils/Constants.kt
+++ app/src/main/java/com/example/videogames/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.videogames.utils
 
 object Constants {
     const val BASE_URL = "https://api.rawg.io/api/"
-    const val API_KEY = "917d62c42c5a422e8eb8b36236118997"
+    const val API_KEY = "YOUR_API_KEY"
     const val PAGE_SIZE = 20
     const val DEFAULT_GENRE = "action"
 }
